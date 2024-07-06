24b49744870a7d1dffb19576be133fcb03426b78
ryanmichael <112053375+conspirathon@users.noreply.github.com>
2024-07-06 01:00:12 -0400

    Add new function: word_count to main.py takes text (from book) as string, returns number of words in string

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    # print(text)
+    num_words = word_count(text)
+    print(f"Number of words: {num_words}")
+
+
+def word_count(text):
+    words = text.split()
+    return len(words)
 
 
 def get_book_text(path):
